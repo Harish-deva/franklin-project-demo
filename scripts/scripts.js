54b907a16b8e96fbb58b3594b27743eec2ecf344
Hasan Mahmud <hasan.mahmud@accenture.com>
2023-05-11 10:24:32 +0200

    hero banner implementation partial

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,11 +21,15 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  */
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
+  const h4 = main.querySelector('h4');
+  const h6 = main.querySelector('h6');
   const picture = main.querySelector('picture');
+  const buttonContainers = Array.from(main.querySelectorAll('p.button-container')).slice(0, 2); 
+
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h6, h1, h4, ...buttonContainers] }));
     main.prepend(section);
   }
 }
